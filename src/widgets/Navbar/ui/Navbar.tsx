ccba0d119e0d123bdaf7fbbf881fda5776bcfaa3
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-02-06 18:30:14 +0100

    feat: Button component, Theme switcher svg

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string
@@ -10,9 +11,10 @@ interface NavbarProps {
 export const Navbar = ({className}:NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={'/'}>Главная</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to={'/'}>Main Page</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>About</AppLink>
             </div>
         </div>
     )
