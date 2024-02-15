39f9a6fc231cc095fea181cb955242622e2cdd15
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-02-15 20:21:26 +0100

    feat: add storybook

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,13 +1,15 @@
-import React, { ButtonHTMLAttributes, FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { ButtonHTMLAttributes, FC } from 'react';
 import cls from './Button.module.scss';
 
 export enum ThemeButton {
-    CLEAR= 'clear'
+    CLEAR = 'clear',
+    OUTLINE = 'outline',
 }
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     className?: string;
-    theme?:ThemeButton
+    theme?: ThemeButton;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
@@ -17,10 +19,11 @@ export const Button: FC<ButtonProps> = (props) => {
         theme,
         ...otherProps
     } = props;
+
     return (
         <button
             type="button"
-            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
             {...otherProps}
         >
             {children}
