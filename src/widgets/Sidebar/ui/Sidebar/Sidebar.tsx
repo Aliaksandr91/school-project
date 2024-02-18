94e8c781002b69efb208b94a6cb84795860bb6fe
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-02-18 12:21:33 +0100

    feat: upgrade sidebar, add button themes

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
-import { useTranslation } from 'react-i18next';
+import React, {useState} from 'react';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
+import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
+import {Button, ButtonSize, ButtonTheme} from 'shared/ui/Button/Button';
+import {useTranslation} from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -25,7 +25,16 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 [className],
             )}
         >
-            <Button data-testid="sidebar-toggle" onClick={onToggle}>{t('toggle')}</Button>
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+                className={cls.collapsedBtn}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                size={ButtonSize.L}
+                square
+            >
+                {collapsed ? '>' : ','}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
