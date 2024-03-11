605195d8946bb8944b5641a78c00131df0294eab
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-03-11 17:00:51 +0100

    feat: add profile, optimization render

---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency {
+    'PLN'= 'PLN',
+    'EUR'= 'EUR',
+    'USD'= 'USD'
+}
+export enum Country {
+    Poland = 'Poland',
+    Belarus = 'Belarus',
+    Ukraine = 'Ukraine',
+    Russia = 'Russia',
+    Kazakhstan = 'Kazahstan',
+    Armenia = 'Armenia',
+}
