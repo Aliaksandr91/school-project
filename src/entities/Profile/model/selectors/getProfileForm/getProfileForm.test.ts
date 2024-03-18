866e24d57c3a8f8b70bda42a146c064b766a88eb
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-03-18 15:47:00 +0100

    feat: add tests for profile

---
+++ src/entities/Profile/model/selectors/getProfileForm/getProfileForm.test.ts
@@ -0,0 +1,28 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { getProfileForm } from './getProfileForm';
+
+describe('getProfileForm', () => {
+    test('should return error', () => {
+        const data = {
+            username: 'admin',
+            age: 32,
+            country: Country.Poland,
+            lastname: 'Horbach',
+            first: 'Aliaksandr',
+            city: 'Wroclaw',
+            currency: Currency.PLN,
+        };
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                form: data,
+            },
+        };
+        expect(getProfileForm(state as StateSchema)).toEqual(data);
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileForm(state as StateSchema)).toEqual(undefined);
+    });
+});
