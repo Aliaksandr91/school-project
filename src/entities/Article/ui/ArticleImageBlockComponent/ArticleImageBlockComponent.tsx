5c19e0460944e98a37707e9f27722651045f410a
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-03-29 23:32:39 +0100

    feat: add content to imageBlock

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { Text, TextAlign } from 'shared/ui/Text/Text';
 import cls from './ArticleImageBlockComponent.module.scss';
 import { ArticleImageBlock } from '../../model/types/article';
 
@@ -15,7 +16,10 @@ export const ArticleImageBlockComponent = memo((props: ArticleDetailsProps) => {
 
     return (
         <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
-            ArticleImageBlockComponent
+            <img src={block.src} alt={block.title} className={cls.img} />
+            {block.title && (
+                <Text text={block.title} align={TextAlign.CENTER} />
+            )}
         </div>
     );
 });
