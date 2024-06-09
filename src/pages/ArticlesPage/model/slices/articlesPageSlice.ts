d7de4631cdce718c23f064194fdbc9704e559973
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-06-09 16:01:21 +0200

    feat: add reducers and selectors for ArticlesPage

--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -2,6 +2,7 @@ import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolki
 import { StateSchema } from 'app/providers/StoreProvider';
 import { Article, ArticleView } from 'entities/Article';
 import { ArticlesPageSchema } from 'pages/ArticlesPage';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
 
 const articlesAdapter = createEntityAdapter<Article>({
     selectId: (article) => article.id,
@@ -24,6 +25,24 @@ const articlesPageSlice = createSlice({
         setView: (state, action:PayloadAction<ArticleView>) => {
             state.view = action.payload;
         },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticlesList.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchArticlesList.fulfilled, (
+                state,
+                action: PayloadAction<Article[]>,
+            ) => {
+                state.isLoading = false;
+                articlesAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchArticlesList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
     },
 });
 
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -3,8 +3,17 @@ import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleList, ArticleView } from 'entities/Article';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { articlesPageReducer } from '../../model/slices/articlesPageSlice';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import {
+    getArticlesPageError,
+    getArticlesPageIsLoading,
+    getArticlesPageView,
+} from '../../model/selectors/articlesPageSelectors';
 import cls from './ArticlesPage.module.scss';
+import { articlesPageReducer, getArticles } from '../../model/slices/articlesPageSlice';
 
 interface ArticlesPageProps {
     className?: string;
@@ -17,14 +26,23 @@ const reducers: ReducersList = {
 const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const articles = useSelector(getArticles.selectAll);
+    const isLoading = useSelector(getArticlesPageIsLoading);
+    const view = useSelector(getArticlesPageView);
+    const error = useSelector(getArticlesPageError);
+
+    useInitialEffect(() => {
+        dispatch(fetchArticlesList);
+    });
 
     return (
         <DynamicModuleLoader reducers={reducers}>
             <div className={classNames(cls.ArticlesPage, {}, [className])}>
                 <ArticleList
-                    isLoading={false}
-                    view={ArticleView.BIG}
-                    articles={[]}
+                    isLoading={isLoading}
+                    view={view}
+                    articles={articles}
                 />
             </div>
         </DynamicModuleLoader>
