428dcaf24ad6428c26a333021af17b2a7775b5ac
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-07-04 17:23:46 +0200

    feat: add filter types

--- src/entities/Article/model/types/article.ts
+++ src/entities/Article/model/types/article.ts
@@ -1,5 +1,10 @@
 import { User } from 'entities/User';
 
+export enum ArticleSortField {
+    VIEWS = 'views',
+    TITLE = ' title',
+    CREATED = 'createAt'
+}
 export enum ArticleBlockType {
     CODE='CODE',
     IMAGE='IMAGE',
