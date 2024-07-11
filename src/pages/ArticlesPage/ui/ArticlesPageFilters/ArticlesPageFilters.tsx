bc890f554bbec3276dfafcb44c4fdcb19623fede
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-07-11 18:54:23 +0200

    feat: little refactoring filters

--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -1,14 +1,21 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
-import { ArticleView, ArticleViewSelector } from 'entities/Article';
-import { articlesPageActions } from 'pages/ArticlesPage/model/slices/articlesPageSlice';
+import {
+    ArticleSortField, ArticleSortSelector, ArticleView, ArticleViewSelector,
+} from 'entities/Article';
 import { useSelector } from 'react-redux';
-import { getArticlesPageView } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Select } from 'shared/ui/Select/Select';
 import { Card } from 'shared/ui/Card/Card';
 import { Input } from 'shared/ui/Input/Input';
+import { SortOrder } from 'shared/types';
+import {
+    getArticlesPageOrder,
+    getArticlesPageSort,
+    getArticlesPageView,
+} from '../../model/selectors/articlesPageSelectors';
+import { articlesPageActions } from '../../model/slices/articlesPageSlice';
 import cls from './ArticlesPageFilters.module.scss';
 
 interface ArticlesPageFiltersProps {
@@ -19,15 +26,29 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
     const { className } = props;
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
-
     const view = useSelector(getArticlesPageView);
+    const sort = useSelector(getArticlesPageSort);
+    const order = useSelector(getArticlesPageOrder);
 
     const onChangeView = useCallback((view: ArticleView) => {
         dispatch(articlesPageActions.setView(view));
     }, [dispatch]);
+    const onChangeSort = useCallback((newOrder: SortOrder) => {
+        dispatch(articlesPageActions.setOrder(newOrder));
+    }, [dispatch]);
+    const onChangeOrder = useCallback((newSort: ArticleSortField) => {
+        dispatch(articlesPageActions.setSort(newSort));
+    }, [dispatch]);
+
     return (
         <div className={classNames(cls.ArticlesPageFilters, {}, [className])}>
             <div className={cls.sortWrapper}>
+                <ArticleSortSelector
+                    sort={}
+                    order={}
+                    onChangeOrder={}
+                    onChangeSort={}
+                />
                 <ArticleViewSelector view={view} onViewClick={onChangeView} />
             </div>
             <Card className={cls.search}>
